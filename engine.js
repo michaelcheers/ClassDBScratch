ad86df35bc67bacf7d339abb902c148037350951
LaurieCheers <lauriecheers@gmail.com>
2016-05-28 15:52:57 +0100

    Basic code structure

--- engine.js
+++ engine.js
@@ -1,9 +1,35 @@
 // JavaScript source code
 
+function getHelloWorldTest()
+{
+  return {
+    "type":"native",
+    "native":alert,
+	"args":[{
+	  "type":"literal",
+	  "literal":"Hello, World!"
+	}]
+  }
+}
+
 function compile(value) {
     return value;
 }
 
-function run(value) {
-
+function run(value)
+{
+	switch(value.type)
+	{
+		case "literal":
+			return value.literal;		
+		case "native":
+			var tempArgs = value.args.map(function(v){
+				  return run(v);
+				});
+			return value["native"].apply(null,tempArgs);
+		default:
+			System.alert("Unknown node type "+value.type);
+			return 0;
+			break;
+	}
 }
