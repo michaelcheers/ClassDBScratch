e8b922f69d85b0506ad25dbfa42b35cd006851ea
Michael Cheers <michaelcheers8@gmail.com>
2016-05-29 10:53:53 +0200

    Fixed errors.

--- engine.js
+++ engine.js
@@ -1,34 +1,73 @@
 // JavaScript source code
 
+ClassDBScratch = {};
+ClassDBScratch.TypeError = function (value, expectedType) {
+    this.message = "Expected " + expectedType + ", got " + typeof (value) + ".";
+}
+ClassDBScratch.TypeError.prototype = Error;
+ClassDBScratch.Events =
+    {
+        greenflag: 0,
+        redflag: 1,
+        keypress: 2,
+        keyrelease: 3,
+        click: 4,
+        receivemessage: 5
+    };
+
+function checkTypes(values, expectedTypes) {
+    for (var n = 0; n < values.length; n++) {
+        var itemValue = values[n];
+        var typeValue = expectedTypes[n];
+        if (typeof (itemValue) != typeValue)
+            throw new ClassDBScratch.TypeError(itemValue, typeValue);
+    }
+}
+
 function $if(condition, consequent, alternate) {
+    checkTypes([condition, consequent, alternate], ['boolean', 'function', 'function']);
     if (condition)
         consequent();
     else if (alternate)
         alternate();
 }
 
+function add(a, b) {
+    checkTypes([a, b], ['number', 'number']);
+    return a + b;
+}
+
 function eq(a, b) {
+    checkTypes([a, b], ['number', 'number']);
     return a === b;
 }
 
 function lt(a, b) {
+    checkTypes([a, b], ['number', 'number']);
     return a < b;
 }
 
 function gt(a, b) {
+    checkTypes([a, b], ['number', 'number']);
     return a > b;
 }
 
 function leq(a, b) {
+    checkTypes([a, b], ['number', 'number']);
     return a <= b;
 }
 
 function geq(a, b) {
+    checkTypes([a, b], ['number', 'number']);
     return a >= b;
 }
 
 function compile(value) {
-    return value;
+    for (var item of value)
+    {
+        var eventName = item.name;
+        var eventBody = item.body;
+    }
 }
 
 function run(value) {
