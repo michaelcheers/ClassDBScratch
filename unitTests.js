50cd6ccfe27034746a1ba2d2bd806358bb6d3f0e
Michael Cheers <michaelcheers8@gmail.com>
2018-05-05 09:37:13 +0200

    Change unit tests to typescript.

--- unitTests.js
+++ unitTests.js
@@ -1,237 +1,234 @@
 // JavaScript source code
-/// <reference path = "engine.js" />
-
+/// <reference path = "engine.ts" />
 function setUnitReg(value) {
     registers.push(value);
 }
-
 function areArraysEqual(a, b) {
     var cLength;
-    if ((cLength = a.length) !== b.length) return false;
+    if ((cLength = a.length) !== b.length)
+        return false;
     for (var n = 0; n < cLength; n++) {
-        if (a[n] !== b[n]) return false;
+        if (a[n] !== b[n])
+            return false;
     }
     return true;
 }
-
 function assertArrayEqual(value, assert) {
-    if (assert === void 0) assert = "Assert failed.";
-    if (!areArraysEqual(registers, value)) document.writeln(assert);
+    if (assert === void 0)
+        assert = "Assert failed.";
+    if (!areArraysEqual(registers, value))
+        document.writeln(assert);
 }
-
 var registers = [];
-
 function clearRegister() {
     registers = [];
 }
-
 var unitTests = [
     function () {
         var code = {
-			globalVariables:[],
-			events:[
-			{
-				name: 'greenflag',
-				body:[
-				{
-                    type: "ifStatement",
-					condition: {
-						type: "native",
-						"native": eq,
-						args: [
-							{
-								type: "literal",
-								literal: 7
-							},
-							{
-								type: "literal",
-								literal: 8
-							}
-						]
-					},
-					consequent: {
-						type: "function",
-						blocks: [
-							{
-								type: "native",
-								"native": setUnitReg,
-								args: [
-									{
-										type: "literal",
-										literal: "Hello World"
-									}
-								]
-							}
-						]
-					},
-					alternate:{
-						type: "function",
-						blocks: [
-							{
-								type: "native",
-								"native": setUnitReg,
-								args: [
-									{
-										type: "literal",
-										literal: "Hello World 2"
-									}
-								]
-							},
-							{
-								type: "native",
-								"native": setUnitReg,
-								args: [
-									{
-										type: "literal",
-										literal: "Hello World 3"
-									}
-								]
-							}
-						]
-					}
-				},
+            globalVariables: [],
+            events: [
                 {
-                    type: 'ifStatement',
-                    condition: {
-                        native: eq,
-                        type: 'native',
-                        args: [
-                            {
-                                type: 'literal',
-                                literal: 7
-                            },
-                            {
-                                type: 'literal',
-                                literal:7
-                            }]
-                    },
-                    consequent: {
-                        type: 'function',
-                        blocks: [
+                    name: 'greenflag',
+                    body: [
                         {
-                            type: 'native',
-                            native: setUnitReg,
-                            run: false,
-                            args: ["Hello World 4"]
-                        }]
-                    }
+                            type: "ifStatement",
+                            condition: {
+                                type: "native",
+                                "native": eq,
+                                args: [
+                                    {
+                                        type: "literal",
+                                        literal: 7
+                                    },
+                                    {
+                                        type: "literal",
+                                        literal: 8
+                                    }
+                                ]
+                            },
+                            consequent: {
+                                type: "function",
+                                blocks: [
+                                    {
+                                        type: "native",
+                                        "native": setUnitReg,
+                                        args: [
+                                            {
+                                                type: "literal",
+                                                literal: "Hello World"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            alternate: {
+                                type: "function",
+                                blocks: [
+                                    {
+                                        type: "native",
+                                        "native": setUnitReg,
+                                        args: [
+                                            {
+                                                type: "literal",
+                                                literal: "Hello World 2"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        type: "native",
+                                        "native": setUnitReg,
+                                        args: [
+                                            {
+                                                type: "literal",
+                                                literal: "Hello World 3"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            type: 'ifStatement',
+                            condition: {
+                                native: eq,
+                                type: 'native',
+                                args: [
+                                    {
+                                        type: 'literal',
+                                        literal: 7
+                                    },
+                                    {
+                                        type: 'literal',
+                                        literal: 7
+                                    }
+                                ]
+                            },
+                            consequent: {
+                                type: 'function',
+                                blocks: [
+                                    {
+                                        type: 'native',
+                                        native: setUnitReg,
+                                        run: false,
+                                        args: ["Hello World 4"]
+                                    }
+                                ]
+                            }
+                        }
+                    ]
                 }
-				]
-			}
-			]
+            ]
         };
         runProgram(compile(code));
         assertArrayEqual(["Hello World 2", "Hello World 3", "Hello World 4"], "If else not working.");
     },
     function () {
-        var code =
-            ({
-				globalVariables: [],
-				events: [{
-					name:'greenflag',
-					data:[],
-					body:[
-					{
-						type: "native",
-						"native": setUnitReg,
-						args: [
-						{
-							type: "literal",
-							literal: "Hello World"
-						}]
-					}]
-				}]
-			});
+        var code = ({
+            globalVariables: [],
+            events: [{
+                    name: 'greenflag',
+                    data: [],
+                    body: [
+                        {
+                            type: "native",
+                            "native": setUnitReg,
+                            args: [
+                                {
+                                    type: "literal",
+                                    literal: "Hello World"
+                                }
+                            ]
+                        }
+                    ]
+                }]
+        });
         runProgram(compile(code));
         assertArrayEqual(["Hello World"], "Hello World not working.");
     },
     function () {
-        var code =
-            {
-                globalVariables: [],
-                events: [{
+        var code = {
+            globalVariables: [],
+            events: [{
                     name: 'greenflag',
-                    data: []
-                , body: [{ "native": setUnitReg, args: [{ type: 'literal', literal: "hello world" }], type: "native" }]
+                    data: [],
+                    body: [{ "native": setUnitReg, args: [{ type: 'literal', literal: "hello world" }], type: "native" }]
                 }
-                ]
-            };
+            ]
+        };
         runProgram(compile(code));
         assertArrayEqual(["hello world"], "hello world not working.");
     },
     function () {
-        var code =
-            {
-                globalVariables: [],
-                events: [
-                    {
-                        name: 'greenflag',
-                        data: [],
-                        body:
-                        [
-                            {
-                                name: "v",
-                                setTo:
-                                    {
-                                        literal: "hello world",
-                                        type: "literal"
-                                    },
-                                type: "vardecl"
+        var code = {
+            globalVariables: [],
+            events: [
+                {
+                    name: 'greenflag',
+                    data: [],
+                    body: [
+                        {
+                            name: "v",
+                            setTo: {
+                                literal: "hello world",
+                                type: "literal"
                             },
-                            {
-                                type: "native",
-                                native: setUnitReg,
-                                args: [
-                                    {
-                                        type: "getvar",
-                                        name: "v",
-                                        local:true
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            };
+                            type: "vardecl"
+                        },
+                        {
+                            type: "native",
+                            native: setUnitReg,
+                            args: [
+                                {
+                                    type: "getvar",
+                                    name: "v",
+                                    local: true
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
         runProgram(compile(code));
         assertArrayEqual(['hello world']);
     },
     function () {
-        var code =
-            {
-                globalVariables: ['v'],
-                events: [
-                    {
-                        name: 'greenflag',
-                        data: [],
-                        body: [
-                            {
-                                type: 'setvar',
-                                local: false,
-                                name: 'v',
-                                setTo: 'hello world'
-                            },
-                            {
-                                type: 'native',
-                                native: setUnitReg,
-                                args: [
-                                    {
-                                        type: 'getvar',
-                                        name: 'v',
-                                        local: false
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            };
+        var code = {
+            globalVariables: ['v'],
+            events: [
+                {
+                    name: 'greenflag',
+                    data: [],
+                    body: [
+                        {
+                            type: 'setvar',
+                            local: false,
+                            name: 'v',
+                            setTo: 'hello world'
+                        },
+                        {
+                            type: 'native',
+                            native: setUnitReg,
+                            args: [
+                                {
+                                    type: 'getvar',
+                                    name: 'v',
+                                    local: false
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
         runProgram(compile(code));
         assertArrayEqual(['hello world']);
     }
 ];
-for (var item of unitTests)
-{
+for (var _i = 0, unitTests_1 = unitTests; _i < unitTests_1.length; _i++) {
+    var item = unitTests_1[_i];
     item();
     clearRegister();
 }
+//# sourceMappingURL=unitTests.js.map
---
+++ unitTests.ts
@@ -0,0 +1,237 @@
+// JavaScript source code
+/// <reference path = "engine.ts" />
+
+function setUnitReg(value) {
+    registers.push(value);
+}
+
+function areArraysEqual(a, b) {
+    var cLength;
+    if ((cLength = a.length) !== b.length) return false;
+    for (var n = 0; n < cLength; n++) {
+        if (a[n] !== b[n]) return false;
+    }
+    return true;
+}
+
+function assertArrayEqual(value, assert?: any) {
+    if (assert === void 0) assert = "Assert failed.";
+    if (!areArraysEqual(registers, value)) document.writeln(assert);
+}
+
+var registers = [];
+
+function clearRegister() {
+    registers = [];
+}
+
+var unitTests = [
+    function () {
+        var code = {
+			globalVariables:[],
+			events:[
+			{
+				name: 'greenflag',
+				body:[
+				{
+                    type: "ifStatement",
+					condition: {
+						type: "native",
+						"native": eq,
+						args: [
+							{
+								type: "literal",
+								literal: 7
+							},
+							{
+								type: "literal",
+								literal: 8
+							}
+						]
+					},
+					consequent: {
+						type: "function",
+						blocks: [
+							{
+								type: "native",
+								"native": setUnitReg,
+								args: [
+									{
+										type: "literal",
+										literal: "Hello World"
+									}
+								]
+							}
+						]
+					},
+					alternate:{
+						type: "function",
+						blocks: [
+							{
+								type: "native",
+								"native": setUnitReg,
+								args: [
+									{
+										type: "literal",
+										literal: "Hello World 2"
+									}
+								]
+							},
+							{
+								type: "native",
+								"native": setUnitReg,
+								args: [
+									{
+										type: "literal",
+										literal: "Hello World 3"
+									}
+								]
+							}
+						]
+					}
+				},
+                {
+                    type: 'ifStatement',
+                    condition: {
+                        native: eq,
+                        type: 'native',
+                        args: [
+                            {
+                                type: 'literal',
+                                literal: 7
+                            },
+                            {
+                                type: 'literal',
+                                literal:7
+                            }]
+                    },
+                    consequent: {
+                        type: 'function',
+                        blocks: [
+                        {
+                            type: 'native',
+                            native: setUnitReg,
+                            run: false,
+                            args: ["Hello World 4"]
+                        }]
+                    }
+                }
+				]
+			}
+			]
+        };
+        runProgram(compile(code));
+        assertArrayEqual(["Hello World 2", "Hello World 3", "Hello World 4"], "If else not working.");
+    },
+    function () {
+        var code =
+            ({
+				globalVariables: [],
+				events: [{
+					name:'greenflag',
+					data:[],
+					body:[
+					{
+						type: "native",
+						"native": setUnitReg,
+						args: [
+						{
+							type: "literal",
+							literal: "Hello World"
+						}]
+					}]
+				}]
+			});
+        runProgram(compile(code));
+        assertArrayEqual(["Hello World"], "Hello World not working.");
+    },
+    function () {
+        var code =
+            {
+                globalVariables: [],
+                events: [{
+                    name: 'greenflag',
+                    data: []
+                , body: [{ "native": setUnitReg, args: [{ type: 'literal', literal: "hello world" }], type: "native" }]
+                }
+                ]
+            };
+        runProgram(compile(code));
+        assertArrayEqual(["hello world"], "hello world not working.");
+    },
+    function () {
+        var code =
+            {
+                globalVariables: [],
+                events: [
+                    {
+                        name: 'greenflag',
+                        data: [],
+                        body:
+                        [
+                            {
+                                name: "v",
+                                setTo:
+                                    {
+                                        literal: "hello world",
+                                        type: "literal"
+                                    },
+                                type: "vardecl"
+                            },
+                            {
+                                type: "native",
+                                native: setUnitReg,
+                                args: [
+                                    {
+                                        type: "getvar",
+                                        name: "v",
+                                        local:true
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            };
+        runProgram(compile(code));
+        assertArrayEqual(['hello world']);
+    },
+    function () {
+        var code =
+            {
+                globalVariables: ['v'],
+                events: [
+                    {
+                        name: 'greenflag',
+                        data: [],
+                        body: [
+                            {
+                                type: 'setvar',
+                                local: false,
+                                name: 'v',
+                                setTo: 'hello world'
+                            },
+                            {
+                                type: 'native',
+                                native: setUnitReg,
+                                args: [
+                                    {
+                                        type: 'getvar',
+                                        name: 'v',
+                                        local: false
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            };
+        runProgram(compile(code));
+        assertArrayEqual(['hello world']);
+    }
+];
+for (var item of unitTests)
+{
+    item();
+    clearRegister();
+}
